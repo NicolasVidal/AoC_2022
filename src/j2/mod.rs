76629da0bc842a38a104f5c0c93165852edacd40
NicolasVidal-IsCool <nicolas.vidal@iscool-e.Com>
2022-12-02 06:40:01 +0100

    j2 : quick and dirty

--- src/j2/mod.rs
+++ src/j2/mod.rs
@@ -1,11 +1,57 @@
-use std::ops::Add;
-use std::str::FromStr;
+#[inline(always)]
+pub fn name_to_u16(name: char) -> u16 {
+    match name {
+        'A' => 0,
+        'B' => 1,
+        'C' => 2,
+        'X' => 0,
+        'Y' => 1,
+        'Z' => 2,
+        _ => panic!(),
+    }
+}
 
-use itertools::Itertools;
+#[inline(always)]
+pub fn predicted_shape(prev_input: u16, name: char) -> u16 {
+    match name {
+        'X' => (prev_input + 2) % 3,
+        'Y' => prev_input,
+        'Z' => (prev_input + 1) % 3,
+        _ => panic!("{}", name),
+    }
+}
+
+#[inline(always)]
+fn compute_score(first: u16, second: u16) -> u16 {
+    let winner = if first == ((second + 1) % 3) {
+        0u16
+    } else if second == ((first + 1) % 3) {
+        6u16
+    } else {
+        3u16
+    };
+    second + 1 + winner
+}
+
+#[inline(always)]
+fn get_chars(line: &str) -> (char, char) {
+    let mut chars = line.chars();
+    let a = chars.next().unwrap();
+    chars.next();
+    (a, chars.next().unwrap())
+}
 
 #[allow(unused)]
 pub fn _p1(s: &str) -> usize {
-    42
+    let mut total = 0u16;
+    for line in s.lines() {
+        let (a, b) = get_chars(line);
+        let first = name_to_u16(a);
+        let second = name_to_u16(b);
+
+        total += compute_score(first, second);
+    }
+    total as usize
 }
 
 #[allow(unused)]
@@ -13,9 +59,18 @@ pub fn p1() -> usize {
     _p1(include_str!("j2.txt"))
 }
 
+
 #[allow(unused)]
 pub fn _p2(s: &str) -> usize {
-    42
+    let mut total = 0u16;
+    for line in s.lines() {
+        let (a, b) = get_chars(line);
+        let first = name_to_u16(a);
+        let second = predicted_shape(first, b);
+
+        total += compute_score(first, second);
+    }
+    total as usize
 }
 
 #[allow(unused)]
@@ -32,14 +87,14 @@ mod j2_tests {
     #[test]
     #[allow(unused)]
     fn test_p1() {
-        assert_eq!(24000, _p1(include_str!("j2_test.txt")));
-        assert_eq!(68775, _p1(include_str!("j2.txt")));
+        assert_eq!(15, _p1(include_str!("j2_test.txt")));
+        assert_eq!(14069, _p1(include_str!("j2.txt")));
     }
 
     #[test]
     #[allow(unused)]
     fn test_p2() {
-        assert_eq!(45000, _p2(include_str!("j2_test.txt")));
-        assert_eq!(202585, _p2(include_str!("j2.txt")));
+        assert_eq!(12, _p2(include_str!("j2_test.txt")));
+        assert_eq!(12411, _p2(include_str!("j2.txt")));
     }
 }
