51136101d4dda84242a5b1fc64f960f06ef738e7
NicolasVidal-IsCool <nicolas.vidal@iscool-e.Com>
2022-12-06 06:20:20 +0100

    j6: simple, still no heap allocations, could be more effective, but doing the job

---
+++ src/j6/mod.rs
@@ -0,0 +1,53 @@
+use itertools::Itertools;
+
+fn get_start_position<const MIN_CHARS: usize>(s: &str) -> usize {
+    let mut last_chars = ['$'; MIN_CHARS];
+    for (i, c) in s.lines().next().unwrap().chars().enumerate() {
+        last_chars[i % MIN_CHARS] = c;
+        if last_chars.iter().all_unique() && i >= MIN_CHARS {
+            return i + 1;
+        }
+    }
+    panic!("{}", format!("there were no unique {MIN_CHARS} characters following each other"));
+}
+
+#[allow(unused)]
+pub fn _p1(s: &str) -> usize {
+    get_start_position::<4>(s)
+}
+
+#[allow(unused)]
+pub fn p1() -> usize {
+    _p1(include_str!("j6.txt"))
+}
+
+#[allow(unused)]
+pub fn _p2(s: &str) -> usize {
+    get_start_position::<14>(s)
+}
+
+#[allow(unused)]
+pub fn p2() -> usize {
+    _p2(include_str!("j6.txt"))
+}
+
+#[cfg(test)]
+#[allow(unused)]
+mod j6_tests {
+    #[allow(unused)]
+    use super::*;
+
+    #[test]
+    #[allow(unused)]
+    fn test_p1() {
+        assert_eq!(7, _p1(include_str!("j6_test.txt")));
+        assert_eq!(1929, _p1(include_str!("j6.txt")));
+    }
+
+    #[test]
+    #[allow(unused)]
+    fn test_p2() {
+        assert_eq!(19, _p2(include_str!("j6_test.txt")));
+        assert_eq!(3298, _p2(include_str!("j6.txt")));
+    }
+}
