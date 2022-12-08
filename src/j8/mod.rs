bb432687413ed014333c2796f01156f50df803ad
NicolasVidal-IsCool <nicolas.vidal@iscool-e.Com>
2022-12-08 14:32:04 +0100

    j8 : use small vecs and matrix struct to try to increase performance

--- src/j8/mod.rs
+++ src/j8/mod.rs
@@ -1,43 +1,95 @@
-#[inline(always)]
-fn parse_matrix(s: &str) -> Vec<Vec<u8>> {
-    let mut trees = vec!();
+use std::ops::Index;
 
-    for line in s.lines() {
-        let mut row = vec!();
-        for c in line.chars() {
-            row.push(c as u8);
-        }
-        trees.push(row);
+use smallvec::SmallVec;
+
+#[derive(Default, Clone, Debug)]
+struct Matrix {
+    inner: SmallVec<[u8; 128 * 128]>,
+    rows: usize,
+    cols: usize,
+}
+
+impl Matrix {
+    #[inline(always)]
+    pub fn push(&mut self, c: u8) {
+        self.inner.push(c);
     }
 
-    trees
+    #[inline(always)]
+    pub fn end_row(&mut self) {
+        self.rows += 1;
+        self.cols = if self.rows == 1 { self.inner.len() } else if self.inner.len() % self.cols != 0 { panic!() } else { self.cols }
+    }
+
+    #[inline(always)]
+    pub fn cols(&self) -> usize {
+        self.cols
+    }
+
+    #[inline(always)]
+    pub fn rows(&self) -> usize {
+        self.rows
+    }
+}
+
+impl<'a> Index<usize> for Matrix {
+    type Output = [u8];
+
+    #[inline(always)]
+    fn index(&self, index: usize) -> &Self::Output {
+        let row = index % self.cols;
+        &self.inner[(row * self.cols)..((row + 1) * self.cols)]
+    }
+}
+
+impl<'a> Index<(usize, usize)> for Matrix {
+    type Output = u8;
+
+    #[inline(always)]
+    fn index(&self, (row, col): (usize, usize)) -> &Self::Output {
+        &self.inner[row * self.cols + col]
+    }
+}
+
+#[inline(always)]
+fn parse_matrix(s: &str) -> Matrix {
+    let mut matrix = Matrix::default();
+
+    for line in s.lines() {
+        for c in line.chars() {
+            matrix.push(c as u8 - '0' as u8);
+        }
+        matrix.end_row();
+    }
+
+    matrix
 }
 
 #[allow(unused)]
 pub fn _p1(s: &str) -> usize {
     let trees = parse_matrix(s);
 
-    let height = trees.len();
-    let width = trees.iter().next().unwrap().len();
+    let height = trees.rows();
+    let width = trees.cols();
 
     let mut total = 0;
 
     for row in 0..height {
         for col in 0..width {
-            let tree_height = trees[row][col];
-            if ((row + 1)..height).all(|l| trees[l][col] < tree_height) {
+            let tree_height = trees[(row, col)];
+            if ((row + 1)..height).all(|l| trees[(l, col)] < tree_height) {
                 total += 1;
                 continue;
             }
-            if (0..row).all(|l| trees[l][col] < tree_height) {
+            if (0..row).all(|l| trees[(l, col)] < tree_height) {
                 total += 1;
                 continue;
             }
-            if ((col + 1)..width).all(|l| trees[row][l] < tree_height) {
+            if ((col + 1)..width).all(|l| trees[(row, l)] < tree_height) {
                 total += 1;
                 continue;
             }
-            if (0..col).all(|l| trees[row][l] < tree_height) {
+            if (0..col).all(|l| trees[(row, l)] < tree_height) {
                 total += 1;
                 continue;
             }
@@ -56,8 +108,8 @@ pub fn p1() -> usize {
 pub fn _p2(s: &str) -> usize {
     let trees = parse_matrix(s);
 
-    let height = trees.len();
-    let width = trees.iter().next().unwrap().len();
+    let height = trees.rows;
+    let width = trees.cols;
 
     let mut best_viewing_distance = 0;
 
@@ -69,10 +121,10 @@ pub fn _p2(s: &str) -> usize {
             let mut up = 1;
             let mut down = 1;
 
-            up = ((row + 1)..(height - 1)).take_while(|l| trees[*l][col] < tree_height).count() + 1;
-            down = (1..row).rev().take_while(|l| trees[*l][col] < tree_height).count() + 1;
-            right = ((col + 1)..(width - 1)).take_while(|l| trees[row][*l] < tree_height).count() + 1;
-            left = (1..col).rev().take_while(|l| trees[row][*l] < tree_height).count() + 1;
+            up = ((row + 1)..(height - 1)).take_while(|l| trees[(*l, col)] < tree_height).count() + 1;
+            down = (1..row).rev().take_while(|l| trees[(*l, col)] < tree_height).count() + 1;
+            right = ((col + 1)..(width - 1)).take_while(|l| trees[(row, *l)] < tree_height).count() + 1;
+            left = (1..col).rev().take_while(|l| trees[(row, *l)] < tree_height).count() + 1;
 
             let viewing_distance = right * left * down * up;
 
