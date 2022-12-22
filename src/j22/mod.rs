6f6ab849e86ffb7e5e18d1ab174b899d7cf4fee2
NicolasVidal-IsCool <nicolas.vidal@iscool-e.Com>
2022-12-22 20:04:33 +0100

    j22: soooo ugly ^^ but working :)

--- src/j22/mod.rs
+++ src/j22/mod.rs
@@ -44,7 +44,8 @@ fn print_grid(
 }
 
 fn execute_instruction(r: &mut usize, c: &mut usize, orientation: &mut Orientation,
-                       grid: &SmallVec<[SmallVec<[Cell; 200]>; 200]>, instruction: Instruction) {
+                       grid: &SmallVec<[SmallVec<[Cell; 200]>; 200]>, instruction: Instruction,
+                       on_cube: bool, test_wrapping: bool) {
     match instruction {
         Instruction::Move(v) => {
             let mut move_left_count = v;
@@ -76,30 +77,43 @@ fn execute_instruction(r: &mut usize, c: &mut usize, orientation: &mut Orientati
                     });
                 }
 
+                let mut potential_new_orientation = orientation.clone();
                 if let Some(orientation) = must_wrap {
-                    let (mut row, mut col) = (*r as isize, *c as isize);
-                    loop {
-                        let (ghost_row, ghost_col) = match orientation {
-                            Orientation::Right => { (row, col + 1) }
-                            Orientation::Down => { (row + 1, col) }
-                            Orientation::Left => { (row, col - 1) }
-                            Orientation::Up => { (row - 1, col) }
-                        };
-                        if ghost_col < 0 {
-                            break;
-                        } else if ghost_col as usize >= grid[0].len() {
-                            break;
-                        } else if ghost_row < 0 {
-                            break;
-                        } else if ghost_row as usize >= grid.len() {
-                            break;
-                        } else if grid[ghost_row as usize][ghost_col as usize] == Cell::Void {
-                            break;
+                    if on_cube {
+                        new_row = *r as isize;
+                        new_col = *c as isize;
+                        // dbg!(new_row, new_col, potential_new_orientation.clone());
+                        if test_wrapping {
+                            apply_test_folding(&mut new_row, &mut new_col, &mut potential_new_orientation);
                         } else {
-                            (row, col) = (ghost_row, ghost_col);
+                            apply_folding(&mut new_row, &mut new_col, &mut potential_new_orientation);
                         }
+                        // dbg!(new_row, new_col, potential_new_orientation.clone());
+                    } else {
+                        let (mut row, mut col) = (*r as isize, *c as isize);
+                        loop {
+                            let (ghost_row, ghost_col) = match orientation {
+                                Orientation::Right => { (row, col + 1) }
+                                Orientation::Down => { (row + 1, col) }
+                                Orientation::Left => { (row, col - 1) }
+                                Orientation::Up => { (row - 1, col) }
+                            };
+                            if ghost_col < 0 {
+                                break;
+                            } else if ghost_col as usize >= grid[0].len() {
+                                break;
+                            } else if ghost_row < 0 {
+                                break;
+                            } else if ghost_row as usize >= grid.len() {
+                                break;
+                            } else if grid[ghost_row as usize][ghost_col as usize] == Cell::Void {
+                                break;
+                            } else {
+                                (row, col) = (ghost_row, ghost_col);
+                            }
+                        }
+                        (new_row, new_col) = (row, col);
                     }
-                    (new_row, new_col) = (row, col);
                 }
 
                 match grid[new_row as usize][new_col as usize] {
@@ -108,6 +122,9 @@ fn execute_instruction(r: &mut usize, c: &mut usize, orientation: &mut Orientati
                     }
                     Cell::Empty => {
                         (*r, *c) = (new_row as usize, new_col as usize);
+                        if on_cube {
+                            *orientation = potential_new_orientation;
+                        }
                         move_left_count -= 1;
                     }
                     Cell::Rock => {
@@ -164,7 +181,7 @@ pub fn _p1(s: &str) -> usize {
 
     let mut val = 0;
     for c in s.lines().skip(grid.len() + 1).next().unwrap().chars() {
-    // for c in "10R1L10".chars() {
+        // for c in "10R1L10".chars() {
         match c {
             c if c <= '9' && c >= '0' => {
                 val = val * 10 + (c as u8 - b'0') as isize
@@ -196,7 +213,7 @@ pub fn _p1(s: &str) -> usize {
     let mut orientation = Orientation::Right;
 
     for instruction in instructions.iter() {
-        execute_instruction(&mut r, &mut c, &mut orientation, &grid, instruction.clone());
+        execute_instruction(&mut r, &mut c, &mut orientation, &grid, instruction.clone(), false, false);
         // print_grid(r, c, &grid);
     }
 
@@ -213,18 +230,278 @@ pub fn p1() -> usize {
     _p1(include_str!("j22.txt"))
 }
 
+
+fn apply_test_folding(r: &mut isize, c: &mut isize, orientation: &mut Orientation) {
+    match orientation {
+        Orientation::Right => {
+            if *r >= 0 && *r <= 3 {
+                *r = 11 - *r % 4;
+                *c = 15;
+                *orientation = Orientation::Left;
+                return;
+            }
+            if *r >= 4 && *r <= 7 {
+                *c = 15 - *r % 4;
+                *r = 8;
+                *orientation = Orientation::Down;
+                return;
+            }
+            if *r >= 8 && *r <= 11 {
+                *r = 3 - *r % 4;
+                *c = 11;
+                *orientation = Orientation::Left;
+                return;
+            }
+        }
+        Orientation::Down => {
+            if *c >= 0 && *c <= 3 {
+                *c = 11 - *c % 4;
+                *r = 11;
+                *orientation = Orientation::Up;
+                return;
+            }
+            if *c >= 4 && *c <= 7 {
+                *r = 11 - *c % 4;
+                *c = 8;
+                *orientation = Orientation::Right;
+                return;
+            }
+            if *c >= 8 && *c <= 11 {
+                *c = 3 - *c % 4;
+                *r = 7;
+                *orientation = Orientation::Up;
+                return;
+            }
+            if *c >= 12 && *c <= 15 {
+                *r = 4 + *c % 4;
+                *c = 0;
+                *orientation = Orientation::Right;
+                return;
+            }
+        }
+        Orientation::Left => {
+            if *r >= 0 && *r <= 3 {
+                *c = 4 + *r % 4;
+                *r = 4;
+                *orientation = Orientation::Down;
+                return;
+            }
+            if *r >= 4 && *r <= 7 {
+                *c = 15 - *r % 4;
+                *r = 11;
+                *orientation = Orientation::Up;
+                return;
+            }
+            if *r >= 8 && *r <= 11 {
+                *c = 7 - *r % 4;
+                *r = 7;
+                *orientation = Orientation::Up;
+                return;
+            }
+        }
+        Orientation::Up => {
+            if *c >= 0 && *c <= 3 {
+                *c = 11 - *c % 4;
+                *r = 0;
+                *orientation = Orientation::Down;
+                return;
+            }
+            if *c >= 4 && *c <= 7 {
+                *r = 0 + *c % 4;
+                *c = 8;
+                *orientation = Orientation::Right;
+                return;
+            }
+            if *c >= 8 && *c <= 11 {
+                *c = 3 - *c % 4;
+                *r = 4;
+                *orientation = Orientation::Down;
+                return;
+            }
+            if *c >= 12 && *c <= 15 {
+                *r = 7 - *c % 4;
+                *c = 11;
+                *orientation = Orientation::Left;
+                return;
+            }
+        }
+    }
+}
+
+fn apply_folding(r: &mut isize, c: &mut isize, orientation: &mut Orientation) {
+    match orientation {
+        Orientation::Right => {
+            if *r >= 0 && *r <= 49 {
+                *r = 149 - *r % 50;
+                *c = 99;
+                *orientation = Orientation::Left;
+                return;
+            }
+            if *r >= 50 && *r <= 99 {
+                *c = 100 + *r % 50;
+                *r = 0;
+                *orientation = Orientation::Up;
+                return;
+            }
+            if *r >= 100 && *r <= 149 {
+                *r = 49 - *r % 50;
+                *c = 149;
+                *orientation = Orientation::Left;
+                return;
+            }
+            if *r >= 150 && *r <= 199 {
+                *c = 50 + *r % 50;
+                *r = 149;
+                *orientation = Orientation::Up;
+                return;
+            }
+        }
+        Orientation::Down => {
+            if *c >= 0 && *c <= 49 {
+                *c = 100 + *c % 50;
+                *r = 0;
+                *orientation = Orientation::Down;
+                return;
+            }
+            if *c >= 50 && *c <= 99 {
+                *r = 150 + *c % 50;
+                *c = 49;
+                *orientation = Orientation::Left;
+                return;
+            }
+            if *c >= 100 && *c <= 149 {
+                *r = 50 + *c % 50;
+                *c = 99;
+                *orientation = Orientation::Left;
+                return;
+            }
+        }
+        Orientation::Left => {
+            if *r >= 0 && *r <= 49 {
+                *r = 149 - *r % 50;
+                *c = 0;
+                *orientation = Orientation::Right;
+                return;
+            }
+            if *r >= 50 && *r <= 99 {
+                *c = 0 + *r % 50;
+                *r = 100;
+                *orientation = Orientation::Down;
+                return;
+            }
+            if *r >= 100 && *r <= 149 {
+                *r = 49 - *r % 50;
+                *c = 50;
+                *orientation = Orientation::Right;
+                return;
+            }
+            if *r >= 150 && *r <= 199 {
+                *c = 50 + *r % 50;
+                *r = 0;
+                *orientation = Orientation::Down;
+                return;
+            }
+        }
+        Orientation::Up => {
+            if *c >= 0 && *c <= 49 {
+                *r = 50 + *c % 50;
+                *c = 50;
+                *orientation = Orientation::Right;
+                return;
+            }
+            if *c >= 50 && *c <= 99 {
+                *r = 150 + *c % 50;
+                *c = 0;
+                *orientation = Orientation::Right;
+                return;
+            }
+            if *c >= 100 && *c <= 149 {
+                *c = 0 + *c % 50;
+                *r = 199;
+                *orientation = Orientation::Up;
+                return;
+            }
+        }
+    }
+}
+
 #[allow(unused)]
-pub fn _p2(s: &str) -> usize {
-    for line in s.lines() {}
-    42
+pub fn _p2(s: &str, test_wrapping: bool) -> usize {
+    let mut max_row_size = 0usize;
+    let mut grid: SmallVec<[SmallVec<[Cell; 200]>; 200]> = smallvec![];
+    let mut instructions: SmallVec<[Instruction; 5000]> = smallvec![];
+    for (row, line) in s.lines().enumerate() {
+        if line.is_empty() {
+            break;
+        }
+        grid.push(smallvec![]);
+        for (col, c) in line.chars().enumerate() {
+            grid[row].push(match c {
+                ' ' => Cell::Void,
+                '#' => Cell::Rock,
+                '.' => Cell::Empty,
+                _ => { panic!() }
+            });
+            max_row_size = max_row_size.max(col)
+        }
+    }
+    max_row_size += 1;
+    for row in grid.iter_mut() {
+        for _ in 0..(max_row_size - row.len()) {
+            row.push(Cell::Void);
+        }
+    }
+
+    let mut val = 0;
+    for c in s.lines().skip(grid.len() + 1).next().unwrap().chars() {
+        // for c in "10R1L10".chars() {
+        match c {
+            c if c <= '9' && c >= '0' => {
+                val = val * 10 + (c as u8 - b'0') as isize
+            }
+            'R' => {
+                if val > 0 {
+                    instructions.push(Instruction::Move(val));
+                    val = 0;
+                }
+                instructions.push(Instruction::TurnRight);
+            }
+            'L' => {
+                if val > 0 {
+                    instructions.push(Instruction::Move(val));
+                    val = 0;
+                }
+                instructions.push(Instruction::TurnLeft);
+            }
+            _ => panic!()
+        }
+    }
+    if val > 0 {
+        instructions.push(Instruction::Move(val));
+        val = 0;
+    }
+
+    let mut r = 0;
+    let mut c = grid[0].iter().find_position(|elt| **elt == Cell::Empty).unwrap().0;
+    let mut orientation = Orientation::Right;
+
+    for instruction in instructions.iter() {
+        execute_instruction(&mut r, &mut c, &mut orientation, &grid, instruction.clone(), true, test_wrapping);
+        // print_grid(r, c, &grid);
+    }
+
+    (r + 1) * 1000 + (c + 1) * 4 + match orientation {
+        Orientation::Right => { 0 }
+        Orientation::Down => { 1 }
+        Orientation::Left => { 2 }
+        Orientation::Up => { 3 }
+    }
 }
 
 #[allow(unused)]
 pub fn p2() -> usize {
-    _p2(include_str!("j22.txt"))
+    _p2(include_str!("j22.txt"), false)
 }
-
-// 20538 too high
 
 #[cfg(test)]
 #[allow(unused)]
@@ -242,7 +519,7 @@ mod j22_tests {
     #[test]
     #[allow(unused)]
     fn test_p2() {
-        assert_eq!(42, _p2(include_str!("j22_test.txt")));
-        assert_eq!(42, _p2(include_str!("j22.txt")));
+        assert_eq!(5031, _p2(include_str!("j22_test.txt"), true));
+        assert_eq!(55343, _p2(include_str!("j22.txt"), false));
     }
 }
