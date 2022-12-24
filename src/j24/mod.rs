8a3ad391abac74fb0e4c1231dca8e42d91434be7
NicolasVidal-IsCool <nicolas.vidal@iscool-e.Com>
2022-12-24 12:53:41 +0100

    j24: working on p2, will make the code nicer afterwards

--- src/j24/mod.rs
+++ src/j24/mod.rs
@@ -1,6 +1,5 @@
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
-use std::ops::Range;
 
 struct Cell {
     north_b: bool,
@@ -61,7 +60,7 @@ struct Edge {
 // to each node. This implementation isn't memory-efficient as it may leave duplicate
 // nodes in the queue. It also uses `usize::MAX` as a sentinel value,
 // for a simpler implementation.
-fn shortest_path(adj_list: &Vec<Vec<Edge>>, start: usize, goal_range: Range<usize>) -> Option<usize> {
+fn shortest_path(adj_list: &Vec<Vec<Edge>>, start: usize, goal: usize, periodicity: usize) -> Option<usize> {
     // dist[node] = current shortest distance from `start` to `node`
     let mut dist: Vec<_> = (0..adj_list.len()).map(|_| usize::MAX).collect();
 
@@ -74,7 +73,7 @@ fn shortest_path(adj_list: &Vec<Vec<Edge>>, start: usize, goal_range: Range<usiz
     // Examine the frontier with lower cost nodes first (min-heap)
     while let Some(State { cost, position }) = heap.pop() {
         // Alternatively we could have continued to find all shortest paths
-        if goal_range.contains(&position) { return Some(cost); }
+        if position % periodicity == goal { return Some(cost); }
 
         // Important as we may have already found a better way
         if cost > dist[position] { continue; }
@@ -101,9 +100,7 @@ fn shortest_path(adj_list: &Vec<Vec<Edge>>, start: usize, goal_range: Range<usiz
 #[allow(unused)]
 pub fn _p1(s: &str) -> usize {
     let (rows, cols, periodicity, edges) = compute_graph_and_edges(s);
-
-    shortest_path(&edges, periodicity * rows * cols,
-                  (periodicity * rows * cols + periodicity)..(periodicity * rows * cols + 2 * periodicity)).unwrap()
+    shortest_path(&edges, 1, rows * cols - 2, rows * cols).unwrap()
 }
 
 fn compute_graph_and_edges(s: &str) -> (usize, usize, usize, Vec<Vec<Edge>>) {
@@ -116,10 +113,10 @@ fn compute_graph_and_edges(s: &str) -> (usize, usize, usize, Vec<Vec<Edge>>) {
         grid.push(row)
     }
 
-    let rows = grid.len() - 2;
-    let cols = grid[0].len() - 2;
+    let rows = grid.len();
+    let cols = grid[0].len();
 
-    let periodicity = (rows) * (cols);
+    let periodicity = (rows - 2) * (cols - 2);
 
     let mut world = Vec::with_capacity(periodicity + 1);
     for p in 0..(periodicity + 1) {
@@ -135,7 +132,7 @@ fn compute_graph_and_edges(s: &str) -> (usize, usize, usize, Vec<Vec<Edge>>) {
     for p in 0..(periodicity + 1) {
         for row in 0..rows {
             for col in 0..cols {
-                let cell = &grid[row + 1][col + 1];
+                let cell = &grid[row][col];
                 if cell.blocked {
                     world[p][row][col] = false;
                 }
@@ -143,51 +140,52 @@ fn compute_graph_and_edges(s: &str) -> (usize, usize, usize, Vec<Vec<Edge>>) {
                     let p = p as isize;
                     let mut row = row as isize;
                     let rows = rows as isize;
-                    let new_p = p % rows;
-                    if new_p > row {
-                        row = row + rows
+                    let new_p = p % (rows - 2);
+                    if new_p >= row {
+                        row = row + rows - 2
                     }
                     let (target_row, target_col) = (row - new_p, col);
-                    world[p as usize][target_row as usize][target_col as usize] = false;
+                    world[p as usize][target_row as usize][target_col] = false;
                 }
                 if cell.south_b {
                     let p = p as isize;
                     let mut row = row as isize;
                     let rows = rows as isize;
-                    let new_p = p % rows;
-                    if new_p + row > rows - 1 {
-                        row = row - rows
+                    let new_p = p % (rows - 2);
+                    if new_p + row >= rows - 1 {
+                        row = row - rows + 2
                     }
                     let (target_row, target_col) = (row + new_p, col);
-                    world[p as usize][target_row as usize][target_col as usize] = false;
+                    world[p as usize][target_row as usize][target_col] = false;
                 }
                 if cell.east_b {
                     let p = p as isize;
                     let mut col = col as isize;
                     let cols = cols as isize;
-                    let new_p = p % cols;
-                    if new_p + col > cols - 1 {
-                        col = col - cols
+                    let new_p = p % (cols - 2);
+                    if new_p + col >= cols - 1 {
+                        col = col - cols + 2
                     }
                     let (target_row, target_col) = (row, col + new_p);
-                    world[p as usize][target_row as usize][target_col as usize] = false;
+                    world[p as usize][target_row][target_col as usize] = false;
                 }
                 if cell.west_b {
                     let p = p as isize;
                     let mut col = col as isize;
                     let cols = cols as isize;
-                    let new_p = p % cols;
-                    if new_p > col {
-                        col = col + cols
+                    let new_p = p % (cols - 2);
+                    if new_p >= col {
+                        col = col + cols - 2
                     }
                     let (target_row, target_col) = (row, col - new_p);
-                    world[p as usize][target_row as usize][target_col as usize] = false;
+                    world[p as usize][target_row][target_col as usize] = false;
                 }
             }
         }
     }
 
     // for p in 0..(periodicity + 1) {
+    //     println!("{}", p);
     //     for row in 0..rows {
     //         for col in 0..cols {
     //             print!("{}", match world[p][row][col] {
@@ -200,93 +198,62 @@ fn compute_graph_and_edges(s: &str) -> (usize, usize, usize, Vec<Vec<Edge>>) {
     //     println!();
     // }
 
-    let mut edges = Vec::with_capacity(world.len() * world[0].len() * world[0][0].len() + world.len());
-    for p in 0..(periodicity) {
+    assert_eq!(world[0], world[periodicity]);
+
+    let mut edges = Vec::with_capacity(periodicity * rows * cols);
+    for p in 0..periodicity {
         for row in 0..rows {
             for col in 0..cols {
                 let mut node_edges = Vec::with_capacity(5);
-                let new_p = (p + 1) % periodicity;
+                if world[p][row][col] {
+                    let new_p = (p + 1) % periodicity;
 
-                if row == 0 && col == 0 {
-                    node_edges.push(
-                        Edge {
-                            node: periodicity * rows * cols + new_p,
-                            cost: 1,
-                        }
-                    );
+                    if world[new_p][row][col] {
+                        node_edges.push(
+                            Edge {
+                                node: new_p * rows * cols + row * cols + col,
+                                cost: 1,
+                            }
+                        );
+                    }
+                    if row > 0 && world[new_p][row - 1][col] {
+                        node_edges.push(
+                            Edge {
+                                node: new_p * rows * cols + (row - 1) * cols + col,
+                                cost: 1,
+                            });
+                    }
+                    if row < rows - 1 && world[new_p][row + 1][col] {
+                        node_edges.push(
+                            Edge {
+                                node: new_p * rows * cols + (row + 1) * cols + col,
+                                cost: 1,
+                            });
+                    }
+                    if col > 0 && world[new_p][row][col - 1] {
+                        node_edges.push(
+                            Edge {
+                                node: new_p * rows * cols + row * cols + col - 1,
+                                cost: 1,
+                            });
+                    }
+                    if col < cols - 1 && world[new_p][row][col + 1] {
+                        node_edges.push(
+                            Edge {
+                                node: new_p * rows * cols + row * cols + col + 1,
+                                cost: 1,
+                            });
+                    }
+                }
+                if col == 0 {
+                    assert!(node_edges.is_empty());
                 }
 
-                if row == rows - 1 && col == cols - 1 {
-                    node_edges.push(
-                        Edge {
-                            node: periodicity * rows * cols + new_p + periodicity,
-                            cost: 1,
-                        }
-                    );
-                }
-
-                if world[new_p][row][col] {
-                    node_edges.push(
-                        Edge {
-                            node: new_p * rows * cols + row * cols + col,
-                            cost: 1,
-                        }
-                    );
-                }
-                if row > 0 && world[new_p][row - 1][col] {
-                    node_edges.push(
-                        Edge {
-                            node: new_p * rows * cols + (row - 1) * cols + col,
-                            cost: 1,
-                        });
-                }
-                if row < rows - 1 && world[new_p][row + 1][col] {
-                    node_edges.push(
-                        Edge {
-                            node: new_p * rows * cols + (row + 1) * cols + col,
-                            cost: 1,
-                        });
-                }
-                if col > 0 && world[new_p][row][col - 1] {
-                    node_edges.push(
-                        Edge {
-                            node: new_p * rows * cols + row * cols + col - 1,
-                            cost: 1,
-                        });
-                }
-                if col < cols - 1 && world[new_p][row][col + 1] {
-                    node_edges.push(
-                        Edge {
-                            node: new_p * rows * cols + row * cols + col + 1,
-                            cost: 1,
-                        });
-                }
                 edges.push(node_edges);
             }
         }
     }
 
-    for p in 0..(periodicity) {
-        let new_p = (p + 1) % periodicity;
-        edges.push(vec!(Edge {
-            node: new_p * rows * cols,
-            cost: 1,
-        }, Edge {
-            node: periodicity * rows * cols + new_p,
-            cost: 1,
-        }));
-    }
-
-    for p in 0..(periodicity) {
-        let new_p = (p + 1) % periodicity;
-        edges.push(vec!(Edge {
-            node: new_p * rows * cols + (rows - 1) * cols + (cols - 1),
-            cost: 1,
-        }, Edge {
-            node: periodicity * rows * cols + new_p + periodicity,
-            cost: 1,
-        }));
-    }
     (rows, cols, periodicity, edges)
 }
 
@@ -299,21 +266,23 @@ pub fn p1() -> usize {
 pub fn _p2(s: &str) -> usize {
     let (rows, cols, periodicity, edges) = compute_graph_and_edges(s);
 
-    let first_way_cost = shortest_path(&edges, periodicity * rows * cols,
-                                      (periodicity * rows * cols + periodicity)..(periodicity * rows * cols + 2 * periodicity)).unwrap();
+    let first_way_cost = shortest_path(&edges,
+                                       1,
+                                       rows * cols - 2,
+                                       rows * cols).unwrap();
 
-    let second_way_cost = shortest_path(&edges, periodicity * rows * cols + periodicity + (first_way_cost) % periodicity,
-                                        (periodicity * rows * cols)..(periodicity * rows * cols + periodicity)).unwrap();
+    let second_way_cost = shortest_path(&edges,
+                                        (rows * cols - 2) + (first_way_cost % periodicity) * rows * cols,
+                                       1,
+                                       rows * cols).unwrap();
 
-    let last_way_cost = shortest_path(&edges, periodicity * rows * cols + (first_way_cost + second_way_cost) % periodicity,
-                                      (periodicity * rows * cols + periodicity)..(periodicity * rows * cols + 2 * periodicity)).unwrap();
+    let last_way_cost = shortest_path(&edges,
+                                       1 + ((first_way_cost + second_way_cost) % periodicity) * rows * cols,
+                                       rows * cols - 2,
+                                       rows * cols).unwrap();
 
-    dbg!(periodicity);
-    dbg!(first_way_cost, second_way_cost, last_way_cost);
-    dbg!(first_way_cost + second_way_cost + last_way_cost)
+    first_way_cost + second_way_cost + last_way_cost
 }
-
-// 763 too low
 
 #[allow(unused)]
 pub fn p2() -> usize {
@@ -337,6 +306,6 @@ mod j24_tests {
     #[allow(unused)]
     fn test_p2() {
         assert_eq!(54, _p2(include_str!("j24_test.txt")));
-        assert_eq!(42, _p2(include_str!("j24.txt")));
+        assert_eq!(785, _p2(include_str!("j24.txt")));
     }
 }
